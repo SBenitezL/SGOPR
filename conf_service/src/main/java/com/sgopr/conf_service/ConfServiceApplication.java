8e1ab8e181bef2b894cef67f2c11d9ca7949db67
Santiago <sbenitez1607@gmail.com>
2025-01-01 19:45:10 -0500

    Add: base initial to config service

---
+++ conf_service/src/main/java/com/sgopr/conf_service/ConfServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sgopr.conf_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfServiceApplication.class, args);
+	}
+
+}
